d14283dabb58626d27e2a3a9bcdfdbe41e5f8b65
Bryan Aument <bryaument@gmail.com>
2022-04-02 16:06:54 -0400

    updated to react v18

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import i18n from './i18next/index';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+const app = (
+  <StrictMode>
     <I18nextProvider i18n={i18n}>
       <App />
     </I18nextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>
 );
+
+root.render(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
