260845d6ccde6eaa4b81e26d26d1c4e1189b38ec
Bryan Aument <bryaument@gmail.com>
2022-04-21 19:59:04 -0400

    updated theme for florence

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import './App.scss';
-import i18n from './i18next';
-import { Card } from './components/index';
 import { useTheme } from './contexts/themeContext';
+import Landing from './views/Landing/Landing';
 
 function App() {
-  const [t] = useTranslation();
-  const { theme, setTheme } = useTheme();
-
-  const changeLng = () => {
-    const lng = i18n.language === 'en' ? 'jp' : 'en';
-    document.documentElement.lang = lng;
-    i18n.changeLanguage(lng)
-      .catch((err) => {
-        console.log('something went wrong loading', err);
-      });
-  };
+  const { theme } = useTheme();
 
   useEffect(() => {
     const schema = document.querySelector('meta[name="theme-color"]');
@@ -28,16 +16,8 @@ function App() {
   return (
     <div className={`App ${theme}`}>
       <div>
-        <button className="theme-button" type="button" onClick={() => setTheme('theme')}>🐠</button>
-        <button className="theme-button" type="button" onClick={() => setTheme('theme-dark')}>🌙</button>
-        <button className="theme-button" type="button" onClick={() => setTheme('theme-coffee')}>☕</button>
+        <Landing />
       </div>
-      <Card>
-        <h1>
-          {t('common.welcome')}
-        </h1>
-        <button className="change-language-btn" type="button" onClick={changeLng}>{t('common.change-lng')}</button>
-      </Card>
     </div>
   );
 }
---
+++ src/views/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing.scss';
+
+function Landing() {
+  return (
+    <>
+      Landing Page
+    </>
+  );
+}
+
+export default Landing;
