f78c0b63c19d185ab5fbb1254bb1da170d44e822
Bryan Aument <bryaument@gmail.com>
2022-04-21 21:49:00 -0400

    some basic typograhy updates

--- src/views/Landing/LandingHero/LandingHero.jsx
+++ src/views/Landing/LandingHero/LandingHero.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './landingHero.scss';
 
 function LandingHero() {
+  const [t] = useTranslation();
   return (
     <section className="landing-hero">
-      Landing Hero
+      <h1 className="landing-hero-name font-hg">{t('common.name')}</h1>
     </section>
   );
 }
