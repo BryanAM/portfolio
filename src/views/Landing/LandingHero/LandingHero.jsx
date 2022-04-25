cbb63ae27cfa446e20ea5507d20f4dec3bcf69b1
Bryan Aument <bryaument@gmail.com>
2022-04-25 18:48:13 -0400

    font-family and small style updates

--- src/views/Landing/LandingHero/LandingHero.jsx
+++ src/views/Landing/LandingHero/LandingHero.jsx
@@ -9,8 +9,8 @@ function LandingHero() {
     <section className="landing-hero">
       <Stack flexDirection="column" alignItems="flex-start">
         <h1 className="landing-hero-name font-size-700">{t('common.name')}</h1>
-        <h2 className="landing-hero-title font-size-400">{t('common.title')}</h2>
-        <p className="landing-hero-intro font-size-300">{t('landing.hero.intro')}</p>
+        <h2 className="landing-hero-title font-size-300">{t('common.title')}</h2>
+        <p className="landing-hero-intro font-size-200">{t('landing.hero.intro')}</p>
       </Stack>
     </section>
   );
