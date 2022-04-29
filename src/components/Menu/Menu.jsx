b2bd5802c0c236289f5a557709bb864b5c5000db
Bryan Aument <bryaument@gmail.com>
2022-04-29 16:12:54 -0400

    updated favicon

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
 import Stack from '../Stack/Stack';
+import { ReactComponent as MeIcon } from '../../assets/pictograms/me.svg';
 import './menu.scss';
 
 function Menu() {
   const [t] = useTranslation();
   return (
     <nav className="menu">
+      <NavLink to="/"><MeIcon className="menu-icon" /></NavLink>
       <Stack spacing={6}>
-        {/* TODO make home link SVG */}
-        <NavLink to="/">{t('menu.home')}</NavLink>
         <NavLink to="/posts">{t('menu.posts')}</NavLink>
         <NavLink to="/projects">{t('menu.projects')}</NavLink>
         <NavLink to="/code">{t('menu.code')}</NavLink>
