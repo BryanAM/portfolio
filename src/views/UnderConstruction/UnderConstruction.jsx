12021983af34df5cfcc195f3a5ca2a14e3b1cab4
Bryan <bryaument@gmail.com>
2022-08-26 12:59:02 -0400

    added colors and theme to construction page

--- src/views/UnderConstruction/UnderConstruction.jsx
+++ src/views/UnderConstruction/UnderConstruction.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './underConstruction.scss';
+import { ReactComponent as BoxIcon } from '../../assets/pictograms/box-1.2.svg';
 
 function UnderConstruction() {
   const [t] = useTranslation();
   return (
-    <>
+    <section className="under-construction">
       <h1>{t('underConstruction.heading')}</h1>
       <p>{t('underConstruction.description')}</p>
-    </>
+      <BoxIcon className="under-construction-box" />
+      <BoxIcon className="under-construction-box" />
+      <BoxIcon className="under-construction-box" />
+    </section>
   );
 }
 
