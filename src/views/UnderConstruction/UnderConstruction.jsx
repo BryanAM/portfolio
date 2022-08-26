2450acfe6cbed3cbe31f136c9831fede48a76c39
Bryan <bryaument@gmail.com>
2022-08-26 12:14:18 -0400

    created UnderConstruction component

---
+++ src/views/UnderConstruction/UnderConstruction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './underConstruction.scss';
+
+function UnderConstruction() {
+  return (
+    <>
+      Underconstruction
+    </>
+  );
+}
+
+export default UnderConstruction;
