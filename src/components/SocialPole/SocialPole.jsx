17c4221bc578eca02a0c34a8c88eea34de6e9686
Bryan Aument <bryaument@gmail.com>
2022-08-30 10:40:14 -0400

    finished socal pole icons and animations

--- src/components/SocialPole/SocialPole.jsx
+++ src/components/SocialPole/SocialPole.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { ReactComponent as GithubIcon } from '../../assets/pictograms/github.svg';
-import { ReactComponent as EmailIcon } from '../../assets/pictograms/sms.svg';
+import { motion } from 'framer-motion';
+import GithubIcon from '../../assets/pictograms/github.svg';
+import EmailIcon from '../../assets/pictograms/sms.svg';
+import LinkedInIcon from '../../assets/pictograms/linkedin.svg';
+import Stack from '../Stack/Stack';
 import './socialPole.scss';
 
 function SocialPole() {
@@ -9,12 +12,18 @@ function SocialPole() {
   };
 
   return (
-    <div className="social-pole">
-      <a href="https://github.com/BryanAM" aria-label="github link" target="_blank" rel="noopener noreferrer"><GithubIcon className="social-pole-github-icon" /></a>
-      <a href="https://www.linkedin.com/in/bryanaument/" target="_blank" rel="noopener noreferrer">LIn</a>
-      <button className="social-pole-mail-button" type="button" aria-label="email button" onClick={handleOnClick}><EmailIcon className="social-pole-email-icon" /></button>
+    <Stack className="social-pole" verticalSpacing={10} flexDirection="column" alignContent="center">
+      <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} href="https://github.com/BryanAM" className="social-pole-link" aria-label="github link" target="_blank" rel="noopener noreferrer">
+        <img src={GithubIcon} alt="github" className="social-pole-github-icon" />
+      </motion.a>
+      <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} href="https://www.linkedin.com/in/bryanaument/" className="social-pole-link" aria-label="github link" target="_blank" rel="noopener noreferrer">
+        <img src={LinkedInIcon} alt="github" className="social-pole-github-icon" />
+      </motion.a>
+      <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className="social-pole-mail-button" type="button" aria-label="email button" onClick={handleOnClick}>
+        <img src={EmailIcon} alt="email" className="social-pole-email-icon" />
+      </motion.button>
       <hr className="social-pole-bar" />
-    </div>
+    </Stack>
   );
 }
 
