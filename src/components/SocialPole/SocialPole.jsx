c7d882d8f19cae06cfb19da071fc04f13dab8007
Bryan Aument <bryaument@gmail.com>
2022-08-30 10:50:58 -0400

    completed work on social pole

--- src/components/SocialPole/SocialPole.jsx
+++ src/components/SocialPole/SocialPole.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import GithubIcon from '../../assets/pictograms/github.svg';
-import EmailIcon from '../../assets/pictograms/sms.svg';
-import LinkedInIcon from '../../assets/pictograms/linkedin.svg';
+import { ReactComponent as GithubIcon } from '../../assets/pictograms/github.svg';
+import { ReactComponent as EmailIcon } from '../../assets/pictograms/sms.svg';
+import { ReactComponent as LinkedInIcon } from '../../assets/pictograms/linkedin.svg';
 import Stack from '../Stack/Stack';
 import './socialPole.scss';
 
@@ -14,13 +14,13 @@ function SocialPole() {
   return (
     <Stack className="social-pole" verticalSpacing={10} flexDirection="column" alignContent="center">
       <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} href="https://github.com/BryanAM" className="social-pole-link" aria-label="github link" target="_blank" rel="noopener noreferrer">
-        <img src={GithubIcon} alt="github" className="social-pole-github-icon" />
+        <GithubIcon className="social-pole-github-icon" />
       </motion.a>
       <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} href="https://www.linkedin.com/in/bryanaument/" className="social-pole-link" aria-label="github link" target="_blank" rel="noopener noreferrer">
-        <img src={LinkedInIcon} alt="github" className="social-pole-github-icon" />
+        <LinkedInIcon className="social-pole-github-icon" />
       </motion.a>
       <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className="social-pole-mail-button" type="button" aria-label="email button" onClick={handleOnClick}>
-        <img src={EmailIcon} alt="email" className="social-pole-email-icon" />
+        <EmailIcon className="social-pole-email-icon" />
       </motion.button>
       <hr className="social-pole-bar" />
     </Stack>
