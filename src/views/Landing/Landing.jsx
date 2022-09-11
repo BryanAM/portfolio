d97807a199a8c9944a30be19ef689541e29a8da3
Bryan Aument <bryaument@gmail.com>
2022-09-11 10:05:02 -0400

    social pole fixed

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import LandingHero from './LandingHero/LandingHero';
+import LandingIntro from './LandingIntro/LandingIntro';
+import { Stack } from '../../components/index';
 import './landing.scss';
 
 function Landing() {
   return (
-    <LandingHero />
+    <Stack className="landing-view" flexDirection="column" alignItems="center">
+      <LandingHero />
+      <LandingIntro />
+    </Stack>
   );
 }
 
