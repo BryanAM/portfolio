f3a5049eccea55682a168a161150d7251416642e
Bryan Aument <bryaument@gmail.com>
2022-09-11 09:51:02 -0400

    added landingIntro component

---
+++ src/views/Landing/LandingIntro/LandingIntro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landingIntro.scss';
+
+function LandingIntro() {
+  return (
+    <section className="landing-intro">
+      test
+    </section>
+  );
+}
+
+export default LandingIntro;
