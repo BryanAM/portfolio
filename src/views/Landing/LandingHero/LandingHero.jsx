03f10bb895ed0ad38831111eeae3ac6887fbbb9e
Bryan Aument <bryaument@gmail.com>
2022-09-25 12:37:51 -0400

    small style updates

--- src/views/Landing/LandingHero/LandingHero.jsx
+++ src/views/Landing/LandingHero/LandingHero.jsx
@@ -7,8 +7,8 @@ import './landingHero.scss';
 function LandingHero() {
   const [t] = useTranslation();
   return (
-    <section className="landing-hero">
-      <Stack className="landing-hero-stack max-content-width" flexDirection="column" alignItems="flex-start">
+    <section className="landing-hero max-content-width">
+      <Stack className="landing-hero-stack" flexDirection="column" alignItems="flex-start">
         <h1 className="landing-hero-name font-size-700">{t('common.name')}</h1>
         <h2 className="landing-hero-title font-size-300">{t('common.title')}</h2>
         <p className="landing-hero-intro font-size-200">{t('landing.hero.intro')}</p>
