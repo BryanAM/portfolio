d8dc8d8076df040bde2d15dc089dee1f996f76a5
Bryan Aument <bryaument@gmail.com>
2022-10-02 18:31:35 -0400

    some design work with image and about section

--- src/views/Landing/LandingIntro/LandingIntro.jsx
+++ src/views/Landing/LandingIntro/LandingIntro.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Stack } from '../../../components/index';
+import Bryan from '../../../assets/pictograms/bryan.jpeg';
 import './landingIntro.scss';
 
 function LandingIntro() {
   return (
     <section className="landing-intro">
       <div className="max-content-width">
-        This is where my intro content will go
+        <Stack flexDirection="row" flexWrap="wrap">
+          <p>Intro Text</p>
+          <img className="landing-intro-picture" src={Bryan} alt="bryan" />
+        </Stack>
       </div>
     </section>
   );
