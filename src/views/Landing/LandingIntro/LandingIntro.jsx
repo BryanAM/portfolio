83f5bbed4aa02c5c1f5a59b24b2c27e53613c368
Bryan Aument <bryaument@gmail.com>
2022-10-03 11:10:51 -0400

    added copy to the intro'

--- src/views/Landing/LandingIntro/LandingIntro.jsx
+++ src/views/Landing/LandingIntro/LandingIntro.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Stack } from '../../../components/index';
 import Bryan from '../../../assets/pictograms/bryan.jpeg';
 import './landingIntro.scss';
 
 function LandingIntro() {
+  const [t] = useTranslation();
   return (
     <section className="landing-intro">
       <div className="max-content-width">
-        <Stack flexDirection="row" flexWrap="wrap">
-          <p>Intro Text</p>
+        <Stack className="landing-intro-stack" flexDirection="column" alignItems="center">
+          <h2 className="landing-intro-header">{t('landing.landingIntro.header')}</h2>
           <img className="landing-intro-picture" src={Bryan} alt="bryan" />
         </Stack>
       </div>
