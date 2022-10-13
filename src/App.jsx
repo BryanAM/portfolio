20cae69402b5e306069beed23d07054d1cc9dd8b
Bryan Aument <bryaument@gmail.com>
2022-10-13 19:25:34 -0400

    resume edits

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useTheme } from './contexts/themeContext';
 import Landing from './views/Landing/Landing';
 import { Menu, Footer } from './components/index';
 import UnderConstruction from './views/UnderConstruction/UnderConstruction';
+import Resume from './views/Resume/Resume';
 
 function App() {
   const { theme } = useTheme();
@@ -21,6 +22,7 @@ function App() {
         <div className={`app-blur-container ${menuOpen}`} />
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/resume" element={<Resume />} />
           <Route path="/*" element={<UnderConstruction />} />
         </Routes>
       </BrowserRouter>
--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Stack } from '../../components/index';
 import './resume.scss';
 
 function Resume() {
+  const { t } = useTranslation();
   return (
-    <Stack>
-      resume goes here
-    </Stack>
+    <div className="max-content-width">
+      <Stack className="resume" flexDirection="column">
+        <Stack className="resume-headers resume-stack" flexDirection="column" alignItems="center">
+          <h1>{t('common.name')}</h1>
+          <h2>{t('common.title')}</h2>
+        </Stack>
+        <Stack className="resume-stack" flexDirection="column">
+          <h3 className="resume-section-header">{t('resume.workExperience')}</h3>
+          {t('resume.jobs', { returnObjects: true }).map((job) => (
+            <>
+              <p>{job.company}</p>
+              <p>{`${job.title}: ${job.dates}`}</p>
+              <ul>
+                {job.experiences.map((experience) => (
+                  <li>{experience}</li>
+                ))}
+              </ul>
+            </>
+          ))}
+          <h3 className="resume-section-header">{t('resume.educationTech')}</h3>
+          <Stack justifyContent="space-between">
+            <Stack flexDirection="column">
+              {t('resume.schools', { returnObjects: true }).map((school) => (
+                <Stack flexDirection="column">
+                  <p>{school.name}</p>
+                  <p>{school.major}</p>
+                  {school.extras.map((extra) => (
+                    <p>{extra}</p>
+                  ))}
+                </Stack>
+              ))}
+            </Stack>
+            <Stack flexDirection="column">
+              {t('resume.techStack', { returnObjects: true }).map((tech) => (
+                <p>{tech}</p>
+              ))}
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </div>
   );
 }
 
