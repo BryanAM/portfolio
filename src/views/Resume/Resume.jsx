8469e22ce15139a51127fbfd493cf1699aa3bc60
Bryan Aument <bryaument@gmail.com>
2022-10-13 20:47:12 -0400

    resume Japanese translation

--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -10,13 +10,13 @@ function Resume() {
       <Stack className="resume" flexDirection="column">
         <Stack className="resume-headers resume-stack" flexDirection="column" alignItems="center">
           <h1>{t('common.name')}</h1>
-          <h2>{t('common.title')}</h2>
+          <h2 className="resume-highlighted-item">{t('common.title')}</h2>
         </Stack>
         <Stack className="resume-stack" flexDirection="column">
-          <h3 className="resume-section-header">{t('resume.workExperience')}</h3>
+          <h3 className="resume-section-header resume-highlighted-item">{t('resume.workExperience')}</h3>
           {t('resume.jobs', { returnObjects: true }).map((job) => (
             <>
-              <p>{job.company}</p>
+              <h4 className="resume-work-header">{job.company}</h4>
               <p>{`${job.title}: ${job.dates}`}</p>
               <ul>
                 {job.experiences.map((experience) => (
@@ -25,12 +25,12 @@ function Resume() {
               </ul>
             </>
           ))}
-          <h3 className="resume-section-header">{t('resume.educationTech')}</h3>
+          <h3 className="resume-stack resume-section-header resume-highlighted-item">{t('resume.educationTech')}</h3>
           <Stack justifyContent="space-between">
             <Stack flexDirection="column">
               {t('resume.schools', { returnObjects: true }).map((school) => (
                 <Stack flexDirection="column">
-                  <p>{school.name}</p>
+                  <h4 className="resume-education-header">{school.name}</h4>
                   <p>{school.major}</p>
                   {school.extras.map((extra) => (
                     <p>{extra}</p>
@@ -44,6 +44,12 @@ function Resume() {
               ))}
             </Stack>
           </Stack>
+          <h3 className="resume-stack resume-section-header resume-highlighted-item">{t('resume.additional')}</h3>
+          <ul>
+            {t('resume.other', { returnObjects: true }).map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
         </Stack>
       </Stack>
     </div>
