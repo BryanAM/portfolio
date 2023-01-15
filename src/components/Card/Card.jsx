0e2f2009d6b03aa2b7670b95777647da516583bd
Bryan Aument <bryaument@gmail.com>
2023-01-15 15:51:19 -0800

    updated feature cards design

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ReactComponent as Blob } from '../../assets/pictograms/blob.svg';
 import './card.scss';
 
-function Card({ children }) {
+function Card({ children, data }) {
   return (
     <div className="card">
-      {children}
+      <div className="card-blob left">
+        <Blob />
+      </div>
+      <div className="card-blob right">
+        <Blob />
+      </div>
+      <div className="card-content-wrapper">
+        <a href={data.url} target="_blank" rel="noreferrer" aria-label="sajeon link">{data.title}</a>
+        <p>{data.description}</p>
+        {children}
+      </div>
     </div>
   );
 }
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
+  data: PropTypes.node.isRequired,
 };
 
 export default Card;
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,49 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { v1 } from 'uuid';
 import { Stack, Card } from '../../components/index';
-import REPO_QUERY from '../../assets/data/githubQuery';
+import projects from '../../assets/data/projects';
 import './projects.scss';
 
 function Projects() {
   const [t] = useTranslation();
-  const [repos, setRepos] = useState();
-
-  const baseUrl = 'https://api.github.com/graphql';
-
-  const headers = {
-    'Content-Type': 'application/json',
-    Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-  };
-
-  useEffect(() => {
-    const fetchResponse = async () => {
-      fetch(baseUrl, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(REPO_QUERY),
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          const gitHubData = res.data.user.repositories.edges;
-          setRepos(gitHubData.map((item) => ({ data: item.node, key: v1() })));
-        });
-    };
-
-    fetchResponse();
-  }, []);
 
   return (
     <div className="max-content-width projects">
       <Stack flexDirection="column">
         <h1 className="posts-header">{t('projects.header')}</h1>
-        {repos && repos.map((repo) => (
-          <Card key={repo.key}>
-            <h1>{repo.data.name}</h1>
-            <p>{repo.data.description}</p>
-            <a href={repo.data.url}>Github</a>
-          </Card>
-        ))}
+        <Stack className="projects-cards-wrapper" flexDirection="row" flexWrap="wrap" gap={16}>
+          {projects && projects.map((project) => (
+            <Card key={project.key} data={project} />
+          ))}
+        </Stack>
       </Stack>
     </div>
   );
