874146003bf15b31632ac7c3d0a1bb34f593fda8
Bryan Aument <bryaument@gmail.com>
2023-01-15 16:55:09 -0800

    tags implements for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Stack from '../Stack/Stack';
 import { ReactComponent as Blob } from '../../assets/pictograms/blob.svg';
 import './card.scss';
 
@@ -15,6 +16,11 @@ function Card({ children, data }) {
       <div className="card-content-wrapper">
         <a className="font-size-300" href={data.url} target="_blank" rel="noreferrer" aria-label="sajeon link">{data.title}</a>
         <p>{data.description}</p>
+        <Stack flexWrap="wrap">
+          {data.tags.map((tag) => (
+            <p className="card-content-tag font-size-100">{tag}</p>
+          ))}
+        </Stack>
         {children}
       </div>
     </div>
