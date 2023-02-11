39811e03f985b34378f3ce26b7956b63b2966c88
Bryan Aument <bryaument@gmail.com>
2023-02-11 12:34:53 -0800

    added staggered intro animation

--- src/views/Landing/LandingHero/LandingHero.jsx
+++ src/views/Landing/LandingHero/LandingHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import { Stack } from '../../../components/index';
 
@@ -6,14 +7,58 @@ import './landingHero.scss';
 
 function LandingHero() {
   const [t] = useTranslation();
+
+  /*
+    Note you need to declare an initial for animate
+    with staggerChildren even if the initial prop isn't here.
+    See parent div for example
+  */
+  const container = {
+    show: {
+      opacity: 1,
+      transition: { staggerChildren: 0.05, delayChildren: 0.1 },
+    },
+  };
+
+  const child = {
+    initial: {
+      opacity: 0,
+      y: -25,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
-    <section className="landing-hero max-content-width">
+    <motion.section
+      className="landing-hero max-content-width"
+      variants={container}
+      initial="initial"
+      animate="show"
+    >
       <Stack className="landing-hero-stack" flexDirection="column" alignItems="flex-start">
-        <h1 className="landing-hero-name font-size-700">{t('common.name')}</h1>
-        <h2 className="landing-hero-title font-size-300">{t('common.title')}</h2>
-        <p className="landing-hero-intro font-size-200">{t('landing.hero.intro')}</p>
+        <motion.h1
+          className="landing-hero-name font-size-700"
+          variants={child}
+        >
+          {t('common.name')}
+        </motion.h1>
+        <motion.h2
+          className="landing-hero-title font-size-300"
+          variants={child}
+        >
+          {t('common.title')}
+        </motion.h2>
+        <motion.p
+          className="landing-hero-intro font-size-200"
+          variants={child}
+        >
+          {t('landing.hero.intro')}
+        </motion.p>
       </Stack>
-    </section>
+    </motion.section>
   );
 }
 
